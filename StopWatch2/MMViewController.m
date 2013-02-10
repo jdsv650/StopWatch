d26f7953398749e29dd95aa628c219f2598b42ce
Jamie <jdsv650@yahoo.com>
2013-02-10 14:30:19 -0600

    Custom font and output label formatting

--- StopWatch2/MMViewController.m
+++ StopWatch2/MMViewController.m
@@ -7,19 +7,21 @@
 //
 
 #import "MMViewController.h"
+#import <math.h>
 
 @interface MMViewController ()
 {
-    IBOutlet UILabel *elapsedTimeLabel;
-    NSTimer *elapsedTimeTimer;
-    double elapsedTime;
-    int buttonPressed;
+    IBOutlet UILabel *elapsedTimeLabel;  //label to display time
+    IBOutlet UIButton *startButton;      //start/stop button
+    NSTimer *elapsedTimeTimer;           //Timer to track elapsed time
+    double elapsedTime;                  //elapsed time as double
+    int buttonPressed;                   //flag start/stop button
 }
 
 
--(IBAction)startStopTimer:(id)sender;
+-(IBAction)startStopTimer:(id)sender;   //start/stop the timer
 -(IBAction)resetTimer:(id)sender;
--(void)updateTime:(NSTimer *) tmr;
+-(void)updateTime:(NSTimer *) tmr;      //called at timer interval
 
 @end
 
@@ -28,20 +30,24 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];    
-    buttonPressed = 0;
-    elapsedTime = 0;  //Reset Time
+    [super viewDidLoad];
     
-	// Do any additional setup after loading the view, typically from a nib.
+    buttonPressed = 0;
+    elapsedTime = 0;    //set elapsed time =0
+    // custom font use fontbook name
+    [elapsedTimeLabel setFont: [UIFont fontWithName:@"FFF Tusj" size: 46.0]];
 }
 
+
+// handle start/stop button
 -(IBAction)startStopTimer:(id)sender
 {
     
+   //method to call at preset interval
    SEL sel = @selector(updateTime:);
     
-    /******
-   UIButton *button = (UIButton*)sender;
+    /******  code to tag and identifiy two separate buttons  --------
+    UIButton *button = (UIButton*)sender;
     if (button.tag == 1) {
         if([elapsedTimeTimer isValid]) {
             //do nothing
@@ -62,63 +68,97 @@
     }
         
    // elapsedTimeTimer = [NSTimer timerWithTimeInterval:0.001 target:self selector:sel userInfo:nil repeats:YES];
-     
-     ******/
+     ************************************/
     
+    //need start/stop button to update title
+    UIButton *sButton = (UIButton*)sender;
     
-      UIButton *sbutton = (UIButton*)sender;
-    
-    // if start button
+    // if start button pressed
     if(buttonPressed == 0)  {
        // elapsedTimeLabel.text = @"ON";
         
-        [sbutton  setTitle:@"Stop" forState:UIControlStateNormal];
+        [sButton  setTitle:@"Stop" forState:UIControlStateNormal];
+        [sButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
         
         if([elapsedTimeTimer isValid]) {
-            //do nothing
+            //do nothing if timer is already running
         }
         else { //start timer
-            elapsedTimeTimer = [NSTimer scheduledTimerWithTimeInterval:0.001 target:self selector:sel userInfo:nil repeats:YES];
+            elapsedTimeTimer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:sel userInfo:nil repeats:YES];
         }
-        buttonPressed = 1; //toggle button functionality
+        buttonPressed = 1; //toggle button on
     }
     else   // stop button
     {
        // elapsedTimeLabel.text = @"OFF";
      
-        
-        [sbutton  setTitle:@"Start" forState:UIControlStateNormal];
-        
-        sbutton.backgroundColor = [UIColor redColor];
+        [sButton  setTitle:@"Start" forState:UIControlStateNormal];
+        [sButton setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
         
         [elapsedTimeTimer invalidate];
         elapsedTimeTimer = nil;
         
-        elapsedTimeLabel.text = [NSString stringWithFormat:@"%0.3f",elapsedTime];
+        //elapsedTimeLabel.text = [NSString stringWithFormat:@"%0.2f",elapsedTime];
         
-        buttonPressed =0;  //toggle button functionality
+        buttonPressed =0;  //toggle button off
     }
     
 
 }
 
+// increment elapsed time and format output label
 -(void)updateTime:(NSTimer *) tmr
 {
-    elapsedTime = elapsedTime + 0.001;
+    elapsedTime = elapsedTime + 0.01;
+    
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+    float fractionPart = 0;
+    int tenthsDisplay = 0;
+    double temp = elapsedTime;
+    
+    //get decimal portion by subtracting from the integer value (floor)
+    fractionPart = elapsedTime - (floor(elapsedTime));
+    
+    //get tenths to display
+    tenthsDisplay = fractionPart /0.1;   
+    
+    if(temp >=3600) {
+        hours = floor(temp / 3600);   //get hours
+        temp = temp - (hours * 3600); //decrement seconds in that many hours
+    }
+    else
+        hours = 0;  //no hours display 0
+    
+    if(temp >= 60) {
+        minutes = floor(temp / 60);   //get minutes
+        temp = temp - (minutes * 60); //derement seconds in that many minutes
+    }
+    else
+        minutes = 0;  //no mins display 0
+    
+    seconds = temp;  //seconds remain
+    
+   
+    elapsedTimeLabel.text = [NSString stringWithFormat:@"%2d:%2d:%2d:%2d",hours, minutes, seconds, tenthsDisplay];
+
    // elapsedTimeLabel.text = [[elapsedTimeLabel text] stringByAppendingString:@"A"];
-    elapsedTimeLabel.text = [NSString stringWithFormat:@"%0.3f",elapsedTime];
+   // elapsedTimeLabel.text = [NSString stringWithFormat:@"%0.2f",elapsedTime];
 }
 
 
 -(IBAction)resetTimer:(id)sender {
+    
+    [startButton setTitle:@"Start" forState:UIControlStateNormal];
+    [startButton setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
 
     buttonPressed = 0;
     elapsedTime = 0;
-   [elapsedTimeTimer invalidate];
+    [elapsedTimeTimer invalidate];
     elapsedTimeTimer = nil;
-        
-    elapsedTimeLabel.text = [NSString stringWithFormat:@"%0.3f",elapsedTime];
-    startStopTimer:sender;
+    elapsedTimeLabel.text = [NSString stringWithFormat:@"%2d:%2d:%2d:%2d",0, 0, 0, 0];
+    startStopTimer:sender;  
 
 }
 
